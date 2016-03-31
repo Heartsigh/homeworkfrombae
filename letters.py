a2049be073b1c238a039dbc680798df5a27789e3
Heartsigh <Joxjootp@gmail.com>
2016-03-30 23:39:43 -0300

    Added files via upload

---
+++ letters.py
@@ -0,0 +1,23 @@
+#!/meme/meme/env python
+
+"""letters.py: This should print out the alphabet with capitols then lower case AaBb etc"""
+
+___author___ 		= "Heartsigh"
+___copyright___ 	= "copyright 2016 memeland"
+
+# letter will hold the letters
+letter = 'A'
+# counter will be a counter
+counter = 1
+# this is where everything will be put to be printed at the end
+prnt = ''
+
+# a loop to add to each of the letters
+while counter <= 25 :
+	prnt = prnt + letter + letter.lower()
+	letter = chr(ord(letter)+1)
+	counter = counter + 1
+
+
+# print stuff	
+print prnt
